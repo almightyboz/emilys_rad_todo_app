e420fb56d95fc32b41b0b9d1781a1adb9d7b1728
Emily Bosakowski <emily.bosakowski@vibes.com>
2018-06-28 10:55:29 -0500

    Create models and CRUD actions

---
+++ rad_todo_app/rad_todo_app/Model/Item/ItemFunctions.swift
@@ -0,0 +1,28 @@
+//
+//  ItemFunctions.swift
+//  rad_todo_app
+//
+//  Created by Emily Bosakowski on 6/28/18.
+//  Copyright © 2018 Emily Bosakowski. All rights reserved.
+//
+
+import Foundation
+
+class ItemFunctions {
+    static func createItem(itemModel: ItemModel) {
+        
+    }
+    
+    static func readItems() {
+        
+    }
+    
+    static func updateItem(itemModel: ItemModel) {
+        
+    }
+    
+    static func deleteItem(itemModel: ItemModel) {
+        
+    }
+    
+}
