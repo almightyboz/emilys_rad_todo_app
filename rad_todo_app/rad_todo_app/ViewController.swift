9fbee77a6e1c762c032166e34e1c75d516081781
Emily Bosakowski <emily.bosakowski@vibes.com>
2018-06-28 10:31:40 -0500

    Make basic UI, TODO Add test list items and ability to create new items

--- rad_todo_app/rad_todo_app/ViewController.swift
+++ rad_todo_app/rad_todo_app/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var tableView: UITableView!
+    
+    func updateTableView(){
+        self.tableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
