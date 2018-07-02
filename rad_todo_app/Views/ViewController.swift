4a28834e740030cf497105433982709f3e982ef3
Emily Bosakowski <emily.bosakowski@vibes.com>
2018-07-02 17:47:48 -0500

    don't show test comments

--- rad_todo_app/Views/ViewController.swift
+++ rad_todo_app/Views/ViewController.swift
@@ -23,12 +23,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         ItemFunctions.readItems(completion:  {
             self.itemsIndexView.reloadData()
         })
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
